ff167f2fbe60cafb88064f26a9a7f4162e449075
hamRS <doriham123@gmail.com>
2024-11-03 15:00:22 -0300

    feat: add shopping cart UI

--- lib/features/sales/presentation/screens/sales_screen.dart
+++ lib/features/sales/presentation/screens/sales_screen.dart
@@ -9,27 +9,146 @@ class SalesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          iconTheme: const IconThemeData(
-            color: Colors.white,
-          ),
-          title: const Text(
-            'Shopping cart',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w400,
-              fontSize: 20,
-            ),
-          ),
-          centerTitle: true,
-          elevation: 1,
-          backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        iconTheme: const IconThemeData(
+          color: Colors.white,
         ),
-        body: Column(
-          children: [
-            SalesProductCardWidget(),
-            SalesProductCardWidget(),
+        title: const Text(
+          'Shopping cart',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w400,
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 1,
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: const Column(
+        children: [
+          SalesProductCardWidget(),
+          SalesProductCardWidget(),
+        ],
+      ),
+      bottomSheet: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 5,
+              spreadRadius: 0.5,
+            ),
           ],
-        ));
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Your shopping cart',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Items: ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '2',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Total: ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '1000GS.',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                style: const ButtonStyle(
+                  fixedSize: WidgetStatePropertyAll(
+                    Size(
+                      double.infinity,
+                      20,
+                    ),
+                  ),
+                ),
+                onPressed: () {},
+                child: const SizedBox.expand(
+                  child: Center(
+                    child: Text(
+                      'confirm',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                style: const ButtonStyle(
+                  fixedSize: WidgetStatePropertyAll(
+                    Size(
+                      double.infinity,
+                      20,
+                    ),
+                  ),
+                  backgroundColor: WidgetStatePropertyAll(
+                    Colors.white,
+                  ),
+                ),
+                onPressed: () {},
+                child: const SizedBox.expand(
+                  child: Center(
+                    child: Text(
+                      'Edit cart',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
