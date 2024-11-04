9d9935490000dc0af3081ba3073ffedf98c8578d
Alejandra Lezcano <ma.alexa2000@fpuna.edu.py>
2024-11-03 21:52:00 -0300

    entidad producto

---
+++ lib/features/categories/domain/entity/product_entity.dart
@@ -0,0 +1,13 @@
+class Product {
+  final int idProducto;
+  final String nombre;
+  final int idCategoria;
+  final double precioVenta;
+
+  Product({
+    required this.idProducto,
+    required this.nombre,
+    required this.idCategoria,
+    required this.precioVenta,
+  });
+}
